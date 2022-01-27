ce9ff0d4c339bd50185b493a32690571b3ce0920
saribalazs <71698313+saribalazs@users.noreply.github.com>
2022-01-27 09:38:08 +0000

    függvény javítása

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 '''
 
 def hossz():
-    if len(tomb[0]) > len(tomb(1)) > len(tomb(2)):
+    if len(tomb[0]) > len(tomb(1)) and len(tomb[0])> len(tomb(2)):
         return f"A leghosszabb szó: {tomb[0]}"
-    elif len(tomb[0]) < len(tomb(1)) < len(tomb(2)):
+    elif len(tomb[0]) < len(tomb(2)) and len(tomb[1]) < len(tomb(2)):
         return f"A leghosszabb szó: {tomb[2]}"
-    elif len(tomb[0]) < len(tomb(1)) > len(tomb(2)):
+    elif len(tomb[0]) < len(tomb(1)) and len(tomb[1]) > len(tomb(2)):
         return f"A leghosszabb szó: {tomb[1]}"
     else:
         return "Az összes szó ugyan olyan hosszú!"
