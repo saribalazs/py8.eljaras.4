4d563e5391ca1ca21b4b6b6137c3db2db466e1e0
saribalazs <71698313+saribalazs@users.noreply.github.com>
2022-01-27 09:35:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+4. Feladat
+Írj egy programot, amely a felhasználótól bekér 3 szót, ezeket egy listában tárolja, és egy eljárás segítségével meghatározza, és a képernyőre kiírja, melyik a legrövidebb szó!
+'''
+
+def hossz():
+    if len(tomb[0]) > len(tomb(1)) > len(tomb(2)):
+        return f"A leghosszabb szó: {tomb[0]}"
+    elif len(tomb[0]) < len(tomb(1)) < len(tomb(2)):
+        return f"A leghosszabb szó: {tomb[2]}"
+    elif len(tomb[0]) < len(tomb(1)) > len(tomb(2)):
+        return f"A leghosszabb szó: {tomb[1]}"
+    else:
+        return "Az összes szó ugyan olyan hosszú!"
+
+
+tomb = []
+for i in range(3):
+    szo = input("Kérem adjon meg egy szót!")
+    tomb.append(szo)
+print(tomb)
+print(hossz())
+
